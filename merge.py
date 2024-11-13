ced6d7859051e0c1f80430f94e8f8fbbfe6b3b13
Jenny Shen <jennyshen@Jennys-MacBook-Pro.local>
2024-11-13 10:02:58 -0800

    update

--- merge.py
+++ merge.py
@@ -17,21 +17,11 @@ base_model = AutoModelForSequenceClassification.from_pretrained(
     base_model_name,
     num_labels=1,
     # torch_dtype=torch.bfloat16,
-    use_flash_attention_2=True,
+    # use_flash_attention_2=True,
 )
 tokenizer = AutoTokenizer.from_pretrained(
     "meta-llama/Llama-3.1-8B-Instruct", use_fast=False
 )
-
-# Adjusted according to the base model
-# Need to do this for the models that don't have an official pad token.
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.pad_token_id = tokenizer.eos_token_id
-tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-print(tokenizer.padding_side)
-tokenizer.truncation_side = "left"
-tokenizer.model_max_length = 4096
-tokenizer.padding_side = "right"
 
 base_model.config.pad_token_id = tokenizer.pad_token_id
 base_model.resize_token_embeddings(len(tokenizer))
