b63afe208d011efdf6a1b6cce0f5cf8ea7e7d74d
JennyShen056 <112578566+JennyShen056@users.noreply.github.com>
2025-06-02 14:35:44 -0700

    Update qwen25_7B_eng_rm.py

--- bradley-terry-rm/qwen25_7B_eng_rm.py
+++ bradley-terry-rm/qwen25_7B_eng_rm.py
@@ -45,10 +45,10 @@ class ScriptArguments:
             "help": "Path to deepspeed config if using deepspeed. You may need this if the model that you want to train doesn't fit on a single GPU."
         },
     )
-    per_device_train_batch_size: Optional[int] = field(default=2)
-    per_device_eval_batch_size: Optional[int] = field(default=2)
+    per_device_train_batch_size: Optional[int] = field(default=4)
+    per_device_eval_batch_size: Optional[int] = field(default=4)
     # for 8 GPU, the global batch size is 512
-    gradient_accumulation_steps: Optional[int] = field(default=8)
+    gradient_accumulation_steps: Optional[int] = field(default=16)
     learning_rate: Optional[float] = field(default=1e-5)
     weight_decay: Optional[float] = field(default=0.001)
     model_name: Optional[str] = field(
